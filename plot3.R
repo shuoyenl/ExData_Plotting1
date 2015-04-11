1cc7ed90b530ec2f6f790132576e1e0b3788a372
shuoyen <shuoyen@hotmail.com>
2015-04-11 15:32:23 -0700

    v2

--- plot3.R
+++ plot3.R
@@ -2,9 +2,9 @@ if (!file.exists("household_power_consumption.txt"))
 {
     unzip("exdata-data-household_power_consumption.zip")
 }
-partialData <- read.table(sourcefile, sep=";", header=TRUE, na.strings=c("?"), nrows = 100)
+partialData <- read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings=c("?"), nrows = 100)
 classes <- sapply(partialData, class)
-allData <- read.table(sourcefile, sep=";", header=TRUE, na.strings=c("?"), colClasses=classes, skip = 0)
+allData <- read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings=c("?"), colClasses=classes, skip = 0)
 sampleData <- allData[allData$Date == '1/2/2007' | allData$Date == '2/2/2007',]
 
 png(filename='plot3.png', width = 480, height = 480)
--- plot4.R
+++ plot4.R
@@ -2,16 +2,16 @@ if (!file.exists("household_power_consumption.txt"))
 {
     unzip("exdata-data-household_power_consumption.zip")
 }
-partialData <- read.table(sourcefile, sep=";", header=TRUE, na.strings=c("?"), nrows = 100)
+partialData <- read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings=c("?"), nrows = 100)
 classes <- sapply(partialData, class)
-allData <- read.table(sourcefile, sep=";", header=TRUE, na.strings=c("?"), colClasses=classes, skip = 0)
+allData <- read.table("household_power_consumption.txt", sep=";", header=TRUE, na.strings=c("?"), colClasses=classes, skip = 0)
 sampleData <- allData[allData$Date == '1/2/2007' | allData$Date == '2/2/2007',]
 
 png(filename='plot4.png', width = 480, height = 480)
 par(mfrow=c(2,2))
 
 #1
-plot(sampleData$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)", xaxt='n')
+plot(sampleData$Global_active_power, type="l", xlab="", ylab="Global Active Power", xaxt='n')
 axis(side=1, at=c(0, 1440, 2880), lab=c("Thu","Fri", "Sat"))
 
 #2
